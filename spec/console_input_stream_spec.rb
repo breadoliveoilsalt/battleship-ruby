e477af9f53315c59a9b234d9aa8a2a0bffe7cecd
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-17 15:14:08 -0400

    Add ConsoleInputStream and tests

---
+++ spec/console_input_stream_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/console_input_stream.rb'
+
+describe ConsoleInputStream do
+
+  let(:user_input) { ConsoleInputStream.new }
+
+  describe "#read_line" do
+
+    it "returns a string corresponding to user input from $stdin" do
+        mock_input = "I, the user, typed this in"
+
+        allow($stdin).to receive(:gets).and_return(mock_input)
+        
+        expect(user_input.read_line).to eq(mock_input)
+    end
+
+  end
+end
