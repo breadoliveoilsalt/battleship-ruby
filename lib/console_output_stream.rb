c3b53a320b20a591679590c17f29deaeaf580cd4
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-17 15:17:04 -0400

    Add ConsoleOutputStream and tests

---
+++ lib/console_output_stream.rb
@@ -0,0 +1,13 @@
+class ConsoleOutputStream
+
+  attr_accessor :output_stream
+
+  def initialize(output_stream: $stdout)
+    @output_stream = output_stream
+  end
+
+  def render(str)
+    output_stream.puts(str)
+  end
+
+end
