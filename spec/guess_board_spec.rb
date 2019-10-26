cf72af00370eb29d619394b3dff59feb463515c2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-10-26 12:30:26 -0400

    Refactor tests for GuessBoard

--- spec/guess_board_spec.rb
+++ spec/guess_board_spec.rb
@@ -5,29 +5,17 @@ require_relative '../lib/guess_response.rb'
 
 describe GuessBoard do
 
-  describe "#new" do
-
-    it "requires a data representation to be passed in" do
-      expect { GuessBoard.new }.to raise_error(ArgumentError)
-    end
-
-  end
-
-  let (:data_double) { instance_double("BoardData") }
-  let (:guess_board) { GuessBoard.new(
-    board_data: data_double
-    ) }
+  let (:guess_board) { GuessBoard.new }
 
   describe "#update_with" do
 
-    it "parses row, column, and hit-status from its arguments coordinate_guess and response, and it passes row, column, and hit-status to data#update_data" do
+    it "parses row, column, and hit-status from its arguments coordinate_guess and response, and updates the data" do
       coordinate_guess = Coordinate.new("a", "1")
       response = instance_double("GuessResponse", "hit?" => true)
-      allow(data_double).to receive(:update_data).with(:a, 0, true).and_return(true)
+      
+      guess_board.update_with(coordinate_guess, response)
 
-      result = guess_board.update_with(coordinate_guess, response)
-
-      expect(result).to eq(true)
+      expect(guess_board.data[:a][0]).to eq(true)
     end
 
   end
