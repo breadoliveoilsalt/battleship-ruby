af4d7caa5057e47df471913af14bdbc64533135d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-05 12:10:49 -0500

    Add HumanPlayer#respond_to_guess with tests

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,4 +1,5 @@
-require_relative '../lib/coordinate.rb'
+require_relative './guess_response.rb'
+require_relative './coordinate.rb'
 
 class HumanPlayer
 
@@ -17,6 +18,18 @@ class HumanPlayer
     row = user_interface.get_row
     column = user_interface.get_column
     Coordinate.new(row, column)
+  end
+
+  def respond_to_guess(coordinate_guess)
+    fleet_board.update_data_with_guess(coordinate_guess)
+    occupying_ship = fleet_board.find_ship(coordinate_guess)
+    if !occupying_ship
+      GuessResponse.respond_with_miss
+    elsif occupying_ship.sunk?
+      GuessResponse.respond_with_hit.add_ship_sunk(occupying_ship.type)
+    else
+      GuessResponse.respond_with_hit
+    end
   end
   
   def note_response(coordinate_guess, guess_response)
