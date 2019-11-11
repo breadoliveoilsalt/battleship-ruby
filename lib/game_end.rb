79b0f59c8ce9c59553b218151ad65803b862ba2b
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-11-11 16:21:00 -0500

    Refactor GameEnd and tests to utilize class methods

--- lib/game_end.rb
+++ lib/game_end.rb
@@ -1,16 +1,6 @@
-require_relative '../lib/console_user_interface.rb'
-require_relative '../lib/console_input_stream.rb'
-require_relative '../lib/console_output_stream.rb'
-
 class GameEnd
 
-  attr_reader :user_interface
-
-  def initialize(user_interface)
-    @user_interface = user_interface
-  end
-
-  def handle_game_over(winner)
+  def self.handle_game_over(user_interface, winner)
     user_interface.announce_winner(winner)
     user_interface.get_play_again
   end 
